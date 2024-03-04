16b3b9eeff88a47547c6dead2b05894286fe9fab
Gaute Meek Olsen <gaute.meek@gmail.com>
2024-03-04 14:08:58 +0100

    link to github and link to site in readme

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # Frontend Framework Chooser
 
+Visit at [frontend-framework-chooser.pages.dev](https://frontend-framework-chooser.pages.dev/)
+
 Looking for a framework to choose for your next project, use this site help with choosing the correct one.
 
 Created for my talk at Kode24-dagen 3.0.
--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,17 @@
     <router-view></router-view>
   </main>
   <footer>
-    Created by Gaute Meek Olsen
+    <p>Created by Gaute Meek Olsen</p>
+    <a href="https://github.com/gautemo/frontend-framework-chooser" target="_blank">Source code</a>
   </footer>
 </template>
+
+<style>
+footer {
+  text-align: center;
+}
+
+footer p {
+  margin: 0;
+}
+</style>
